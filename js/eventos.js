517fd6984ec50f9e0051018804f98f06b11ac8e4
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-07-24 14:35:19 -0300

    Teste de modularização

---
+++ js/eventos.js
@@ -0,0 +1,32 @@
+
+const listaBtn = document.querySelectorAll('button');
+let teclasPressionadas = {};
+
+for (let i = 0; i < listaBtn.length; i++) {
+    listaBtn[i].onclick = function () {
+        let idAudio = `#som_${listaBtn[i].classList[1]}`;
+        document.querySelector(idAudio).currentTime = 0;
+        document.querySelector(idAudio).play();
+    };
+}
+
+window.onkeydown = function (event) {
+    if (teclasPressionadas[event.key])
+        return;
+    for (let i = 0; i < listaBtn.length; i++) {
+        if (listaBtn[i].classList[2] == event.key) {
+            listaBtn[i].onclick();
+            listaBtn[i].classList.add('ativa');
+        }
+    }
+    teclasPressionadas[event.key] = true;
+}
+
+window.onkeyup = function (event) {
+    for (let i = 0; i < listaBtn.length; i++) {
+        if (listaBtn[i].classList[2] == event.key) {
+            listaBtn[i].classList.remove('ativa');
+        }
+    }
+    teclasPressionadas[event.key] = false;
+}
